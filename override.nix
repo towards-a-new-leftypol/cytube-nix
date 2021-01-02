f7dcee72774eb031ad5d0c38da59643b392c5c0e
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2021-01-02 04:25:31 -0500

    Read config.yaml filepath from env variable
    - patch sync/src/config.js to attempt to read the config.yaml file
    from a location specified by an environment variable
    - this approach will be scrapped in favor of forking sync.git

--- override.nix
+++ override.nix
@@ -6,15 +6,23 @@ let
   nodePackages = import ./node2nix_generated/default.nix {
     inherit pkgs system;
   };
+
+  # everything to do with this bloody patch file is an ugly hack
+  # because node2nix only gives us the preRebuild to override and we
+  # have to implement patching in this phase ourselves.
+  patchFile = builtins.readFile ./configfile_env.patch;
 in
 nodePackages // {
-  package = (builtins.trace "nodePackages:\n${builtins.toJSON nodePackages}" nodePackages).package.override {
+  package = nodePackages.package.override {
     buildInputs = [
-      nodePackages.args.buildInputs
       pkgs.nodePackages.node-pre-gyp
       pkgs.libtool
       pkgs.autoconf
       pkgs.automake
-    ];
+    ] ++ nodePackages.args.buildInputs;
+
+    preRebuild = ''
+      echo '${patchFile}' | patch -p0
+    '';
   };
 }
